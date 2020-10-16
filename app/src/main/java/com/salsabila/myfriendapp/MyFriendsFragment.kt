2106c94760a90c756fd91b6bdc16f1e934acd312
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2020-10-16 15:21:42 +0700

    first commit

---
+++ app/src/main/java/com/salsabila/myfriendapp/MyFriendsFragment.kt
@@ -0,0 +1,90 @@
+package com.salsabila.myfriendapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.my_friend_fragment.*
+
+class MyFriendsFragment : Fragment() {
+    companion object {
+        fun newInstance(): MyFriendsFragment {
+            return MyFriendsFragment()
+        }
+    }
+
+    private var listTeman: List<MyFriend>? = null
+    private var db: AppDatabase? = null
+    private var myFriendDao: MyFriendDAO? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container:
+        ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.my_friend_fragment,
+            container, false
+        )
+    }
+
+    override fun onViewCreated(
+        view: View, savedInstanceState:
+        Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initLocalDB()
+        initView()
+    }
+
+    private fun initLocalDB() {
+        db = AppDatabase.getAppDataBase(activity!!)
+        myFriendDao = db?.myFriendDao()
+    }
+
+    private fun initView() {
+        fabAddFriend.setOnClickListener {
+            (activity as
+                    MainActivity).tampilMyFriendsAddFragment()
+        }
+        ambilDataTeman()
+    }
+
+    private fun ambilDataTeman() {
+        listTeman = ArrayList()
+        myFriendDao?.ambilSemuaTeman()?.observe(this, Observer { r ->
+            listTeman = r
+            when {
+                listTeman?.size == 0 -> tampilToast("Belum ada data teman")
+                else -> {
+                    tampilTeman()
+                }
+            }
+        })
+    }
+
+    private fun tampilToast(message: String) {
+        Toast.makeText(activity!!, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun tampilTeman() {
+        listMyFriends.layoutManager = LinearLayoutManager(activity)
+        listMyFriends.adapter = MyFriendAdapter(activity!!, (listTeman as ArrayList<MyFriend>?)!!)
+    }
+
+    // private fun simulasiDataTeman() {
+    // listTeman = ArrayList()
+    //
+    // listTeman.add(MyFriend("Muhammad", "Laki-laki", "ade@gmail.com",  "085719004268", "Bandung"))
+    // listTeman.add(MyFriend("Al Harits", "Laki laki", "rifaldi@gmail.com", "081213416171", "Bandung"))
+    //
+    // }
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+}
